f27c72c8f1e302630b912a6d7c51a486f6caaf09
Rui Maranhao <rui@computer.org>
2022-01-13 18:53:30 +0000

    more pbt testing

--- src/test/java/CalculatorPBTTests.java
+++ src/test/java/CalculatorPBTTests.java
@@ -5,18 +5,34 @@ import net.jqwik.api.constraints.Positive;
 import pt.up.fe.ldts.NumberInt;
 import pt.up.fe.ldts.NumberOps;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CalculatorPBTTests {
 
     @Property
     public void testSumAssociativity(@ForAll int a, @ForAll int b, @ForAll int c) {
+        System.out.println(a + " " + b + " " + c);
         assert((a + b) + c == a + (b + c));
     }
 
     @Property
     public void testNullAssociativity(@ForAll int a) {
         assert(a + 0 == a);
+        assert(0 + a == a);
+        assert(0 + a == a + 0);
+    }
+
+    @Property
+    public void testDoubleReverse(@ForAll List<Integer> list) {
+        System.out.println(list.toString());
+        assert(reverseList(reverseList(list)).equals(list));
+    }
+
+    public <T> List<T> reverseList(List<T> list) {
+        ArrayList<T> reversed = new ArrayList<>();
+        for (T e : list) reversed.add(0, e);
+        return reversed;
     }
 
     @Property
